6b8893be5288f6cce7e2687b8ed870191ce57a99
Ayush <ayush.srivastava20704@gmail.com>
2026-09-10 19:40:20 +0530

    Created stg_customer view

---
+++ dbt/qcommerce/models/staging/stg_customers.sql
@@ -0,0 +1,10 @@
+SELECT
+    customer_id,
+    TRIM(zone_id) AS zone_id,
+    SAFE_CAST(latitude AS FLOAT64) AS latitude,
+    SAFE_CAST(longitude AS FLOAT64) AS longitude
+FROM {{ source('raw', 'customers') }}
+QUALIFY ROW_NUMBER() OVER (
+    PARTITION BY customer_id
+    ORDER BY customer_id
+) = 1
