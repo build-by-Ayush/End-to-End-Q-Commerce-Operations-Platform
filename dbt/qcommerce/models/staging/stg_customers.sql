da7ef14e08255c9e472eb1ab768e7a1640e9a7c4
Ayush <ayush.srivastava20704@gmail.com>
2026-09-11 19:04:04 +0530

    Created stg_stores view

--- dbt/qcommerce/models/staging/stg_customers.sql
+++ dbt/qcommerce/models/staging/stg_customers.sql
@@ -1,9 +1,12 @@
 SELECT
     customer_id,
     TRIM(zone_id) AS zone_id,
+    
     SAFE_CAST(latitude AS FLOAT64) AS latitude,
     SAFE_CAST(longitude AS FLOAT64) AS longitude
+
 FROM {{ source('raw', 'customers') }}
+
 QUALIFY ROW_NUMBER() OVER (
     PARTITION BY customer_id
     ORDER BY customer_id
---
+++ dbt/qcommerce/models/staging/stg_stores.sql
@@ -0,0 +1,28 @@
+SELECT
+    TRIM(store_id) AS store_id,
+    TRIM(REPLACE(store_name, '#', '')) AS store_name,
+    UPPER(TRIM(REPLACE(zone, '#', ''))) AS zone,
+
+    SAFE_CAST(TRIM(latitude) AS FLOAT64) AS latitude,
+    SAFE_CAST(TRIM(longitude) AS FLOAT64) AS longitude,
+    SAFE_CAST(TRIM(baseline_capacity) AS INT64) AS baseline_capacity,
+
+    UPPER(TRIM(status)) AS status,
+    SAFE.PARSE_DATE('%d-%m-%Y',NULLIF(TRIM(opened_at), '')) AS opened_at,
+
+    CASE
+        WHEN closed_at IS NULL THEN NULL
+        WHEN TRIM(closed_at) = '' THEN NULL
+        WHEN UPPER(TRIM(closed_at)) = 'NULL' THEN NULL
+        ELSE SAFE.PARSE_DATE(
+            '%d-%m-%Y',
+            SUBSTR(TRIM(closed_at), 1, 10)
+        )
+    END AS closed_at
+
+FROM {{ source('raw', 'stores') }}
+
+QUALIFY ROW_NUMBER() OVER (
+    PARTITION BY store_id
+    ORDER BY store_id
+) = 1
