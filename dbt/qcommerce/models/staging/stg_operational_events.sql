ffcb059b6673d2bb29b8fd2d3700a2f14e15a3bc
Ayush <ayush.srivastava20704@gmail.com>
2026-09-13 18:17:42 +0530

    Created stg_operational_events view Completed staging layer

---
+++ dbt/qcommerce/models/staging/stg_operational_events.sql
@@ -0,0 +1,31 @@
+SELECT
+    TRIM(event_id) AS event_id,
+
+    CASE
+        WHEN event_type IS NULL THEN NULL
+        WHEN TRIM(event_type) = '' THEN NULL
+        WHEN UPPER(TRIM(event_type)) = 'NULL' THEN NULL
+        ELSE UPPER(TRIM(REPLACE(event_type, '#', '')))
+    END AS event_type,
+
+    SAFE.PARSE_TIMESTAMP('%d-%m-%Y %H:%M',TRIM(occurred_at)) AS occurred_at,
+
+    TRIM(order_id) AS order_id,
+    TRIM(fulfilment_unit_id) AS fulfilment_unit_id,
+    TRIM(delivery_id) AS delivery_id,
+    TRIM(store_id) AS store_id,
+    TRIM(rider_id) AS rider_id,
+
+    CASE
+        WHEN reason IS NULL THEN NULL
+        WHEN TRIM(reason) = '' THEN NULL
+        WHEN UPPER(TRIM(reason)) = 'NULL' THEN NULL
+        ELSE UPPER(TRIM(REPLACE(reason, '#', '')))
+    END AS reason
+
+FROM {{ source('raw', 'operational_events') }}
+
+QUALIFY ROW_NUMBER() OVER(
+    PARTITION BY event_id
+    ORDER BY event_id
+) = 1
